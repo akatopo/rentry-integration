e7bd9c3b1d6a3ebce24c8d8b0ae8c493cd15c7b6
Alex Katopodis <akatopo@users.noreply.github.com>
2025-11-24 23:36:26 +0200

    use fetch for rentry api calls
    - use application/x-www-form-urlencoded exclusively
    - use ky to wrap fetch
    - use abort signals

--- rentry.ts
+++ rentry.ts
@@ -1,28 +1,48 @@
 // see https://github.com/microsoft/TypeScript/issues/45167 for Error.cause
 
 import { requestUrl } from 'obsidian';
+import ky from 'ky';
 import { parse as parseCookie } from 'cookie';
-// @ts-ignore
-import FormData from 'form-data/lib/form_data.js';
 
-type FormData = import('form-data');
+type CreateRes = {
+  status: string;
+  content: 'OK';
+  url: string;
+  url_short: string;
+  edit_code: string;
+};
+
+type ErrorRes = {
+  status: string;
+  content: string;
+  errors?: string;
+};
+
+type OkRes = { status: '200'; content: 'OK' };
 
 const baseUrl = 'https://rentry.co';
 
-export async function create({ id, text }: { id?: string; text: string }) {
-  const res = (
-    await executeRequest({
-      payload: {
-        text,
-        edit_code: '',
-        url: '',
-      },
-      endpoint: 'api/new',
-    })
-  ).json;
+export async function create({
+  id,
+  text,
+  signal,
+}: {
+  id?: string;
+  text: string;
+  signal?: AbortSignal;
+}) {
+  const res = await executeRequest<CreateRes>({
+    payload: {
+      text,
+      edit_code: '',
+      url: '',
+    },
+    endpoint: 'api/new',
+    signal,
+  });
 
   return {
-    id: getIdFromUrl(res.url),
+    id: String(res.url_short),
     url: String(res.url),
     editCode: String(res.edit_code),
   };
@@ -31,108 +51,111 @@ export async function create({ id, text }: { id?: string; text: string }) {
 export async function remove({
   id,
   editCode,
+  signal,
 }: {
   id: string;
   editCode: string;
+  signal?: AbortSignal;
 }) {
-  return executeRequest({
+  await executeRequest<OkRes>({
     payload: {
       edit_code: editCode,
     },
     endpoint: `/api/delete/${id}`,
+    signal,
   });
 }
 
-// content:  "OK"
-// status:  "200"
-// in res object
 export async function update({
   id,
   editCode,
   text,
+  signal,
 }: {
   id: string;
   editCode: string;
   text: string;
+  signal?: AbortSignal;
 }) {
-  return executeRequest({
+  await executeRequest<OkRes>({
     payload: {
       text,
       edit_code: editCode,
     },
     endpoint: `api/edit/${id}`,
-    useFormUrlEncoded: true,
+    signal,
   });
 }
 
-async function base() {
+function abortablePromise<T>(
+  p: Promise<T>,
+  { signal }: { signal?: AbortSignal },
+) {
+  return new Promise<T>((resolve, reject) => {
+    if (signal?.aborted) {
+      reject(signal.reason);
+    }
+
+    signal?.addEventListener('abort', () => {
+      reject(signal.reason);
+    });
+
+    try {
+      p.then((res) => resolve(res)).catch((reason) => reject(reason));
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+async function fetchCsrfMiddlewareToken({ signal }: { signal?: AbortSignal }) {
   try {
-    const res = await requestUrl(baseUrl);
+    // need to use requestUrl due to CORS
+    const res = await abortablePromise(requestUrl(baseUrl), { signal });
+
     const setCookie = res.headers['set-cookie'][0];
     const cookies = parseCookie(setCookie);
     const { csrftoken: csrfToken } = cookies;
-    const headers = {
-      Referer: baseUrl,
-      Cookie: setCookie,
-    };
+    if (!csrfToken) {
+      throw new Error('Could not set CSRF token');
+    }
 
-    return { csrfToken, headers };
+    return csrfToken;
   } catch (cause) {
     throw new Error('Failed to fetch base cookies', { cause });
   }
 }
 
-async function executeRequest({
+async function executeRequest<T extends Record<string, unknown>>({
   payload,
   endpoint,
-  method = 'POST',
-  useFormUrlEncoded = true, // TODO change this to true and check all endpoints
+  signal,
 }: {
   payload: Record<string, string>;
   endpoint: string;
-  method?: 'POST' | 'GET';
-  useFormUrlEncoded?: boolean;
+  signal?: AbortSignal;
 }) {
-  const { headers: baseHeaders, csrfToken: csrfmiddlewaretoken } = await base();
-  const form = createForm({
-    csrfmiddlewaretoken,
-    ...payload,
-  });
-
   const headers = {
-    ...baseHeaders,
-    ...(useFormUrlEncoded
-      ? { 'Content-Type': 'application/x-www-form-urlencoded' }
-      : form.getHeaders()),
+    'Content-Type': 'application/x-www-form-urlencoded',
   };
 
-  const body = useFormUrlEncoded
-    ? String(new URLSearchParams({ csrfmiddlewaretoken, ...payload }))
-    : form.getBuffer().toString();
-
   try {
-    const res = await requestUrl({
-      url: `${baseUrl}/${endpoint}`,
-      headers,
-      method,
-      body,
-    });
-    return res;
+    const csrfmiddlewaretoken = await fetchCsrfMiddlewareToken({ signal });
+    const body = String(
+      new URLSearchParams({ csrfmiddlewaretoken, ...payload }),
+    );
+    const res = await ky
+      .post<T | ErrorRes>(`${baseUrl}/${endpoint}`, {
+        headers,
+        body,
+        signal,
+      })
+      .json();
+    if (Object.hasOwn(res, 'errors') || res.content !== 'OK') {
+      throw new Error((res as ErrorRes).content);
+    }
+    return res as T;
   } catch (cause) {
-    throw new Error(`Failed to execute ${method} ${endpoint}`, { cause });
+    throw new Error(`Failed to execute POST ${endpoint}`, { cause });
   }
-}
-
-function createForm(data: Record<string, string>) {
-  const form: FormData = new FormData();
-
-  Object.entries(data).forEach(([name, value]) => {
-    form.append(name, value);
-  });
-
-  return form;
-}
-
-function getIdFromUrl(url: string) {
-  return new URL(url).pathname.replaceAll('/', '');
 }
