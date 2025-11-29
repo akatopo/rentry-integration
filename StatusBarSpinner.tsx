0f5a00e47e6ec076e305c50a99b6432f2cf5f6e9
Alex Katopodis <akatopo@users.noreply.github.com>
2025-11-29 22:43:51 +0200

    change spinner to something more obsidian-like

--- StatusBarSpinner.tsx
+++ StatusBarSpinner.tsx
@@ -1,13 +1,28 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { h, Fragment } from './h.js';
 
-import { getIcon } from 'obsidian';
+const spinner = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="svg-icon"
+  >
+    <path d="m 15.174668,3.5621906 a 9.013958,9.0154591 0 0 1 5.82565,7.9420234" />
+  </svg>
+);
 
 export function StatusBarSpinner({ label }: { label: string }) {
   return (
     <>
       <span class="status-bar-item-icon status-bar-item-segment">
-        {getIcon('loader-circle')}
+        {spinner}
       </span>
       <span class="status-bar-item-segment">{label}</span>
     </>
